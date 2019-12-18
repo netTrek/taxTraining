8e34266687be7217dd5f90a37e9f3f915ecbbf52
Saban Uenlue - netTrek <us@netTrek.de>
2019-12-18 11:48:03 +0100

    dangerBtn completed

--- src/app/utils/danger-btn/danger-btn.directive.ts
+++ src/app/utils/danger-btn/danger-btn.directive.ts
@@ -1,18 +1,36 @@
-import { Directive, HostBinding } from '@angular/core';
+import { Directive, EventEmitter, HostBinding, HostListener, Input, Output } from '@angular/core';
+import { aliasTransformFactory } from '@angular/compiler-cli/src/ngtsc/transform/src/alias';
 
-@Directive({
+@Directive ( {
   selector: 'button[taxDangerBtn]'
-})
+} )
 export class DangerBtnDirective {
 
-  @HostBinding ('style.backgroundColor')
+  @HostBinding ( 'style.backgroundColor' )
   backgroundColor = 'red';
 
-
-  @HostBinding ('style.fontWeight')
+  @HostBinding ( 'style.fontWeight' )
   fontWeight = 'bold';
 
+  @Output()
+  confirmed: EventEmitter<undefined> = new EventEmitter();
+
+  @Input()
+  taxDangerBtn: string;
+
+  @Input()
+  taxDangerBtnCounter: number;
+
   constructor() {
+  }
+
+  @HostListener ( 'click' )
+  directiveWasClicked() {
+    const msg = this.taxDangerBtn || 'do you really wand to do that?';
+    // console.log ( this.taxDangerBtnCounter );
+    if ( confirm( msg ) ) {
+      this.confirmed.emit();
+    }
   }
 
 }
