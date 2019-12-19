6c4ca57c1cbde5a7906b1d0b938ccbf88c8dcaab
Saban Uenlue - netTrek <us@netTrek.de>
2019-12-19 09:48:58 +0100

    userlist as behaviourSub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { AppServiceToDeleteLater } from './app.service.to-delete-later';
 import { UserService } from './user/user.service';
 import { interval } from 'rxjs';
-import { take } from 'rxjs/operators';
+import { distinctUntilChanged, filter, take } from 'rxjs/operators';
 
 /**
  * WIFI: Hotel Albert
@@ -20,10 +20,17 @@ export class AppComponent {
 
   constructor( public $myService: AppServiceToDeleteLater, $user: UserService ) {
     console.log ( $myService );
-    interval ( 100 )
+    interval ( 500 )
       .pipe ( take ( 5 ) )
       .subscribe ( value => {
         $user.add ( { firstname: 'saban' + value, lastname: 'ünlü' + value } );
       } );
+    $user.userList$
+         .pipe(
+           filter( value => !!value )
+         )
+         .subscribe(
+            newList => console.log ( 'list changes to', newList )
+         );
   }
 }
--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,38 +1,43 @@
 import { Injectable } from '@angular/core';
 import { User } from './user';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
+  /*
   userList: User[] = [
-    {firstname: 'saban', lastname: 'ü'}/*,
-    {firstname: 'peter', lastname: 'm'}*/
+    {firstname: 'saban', lastname: 'ü'}
   ];
+  */
+  userList$: BehaviorSubject<User[]> = new BehaviorSubject([
+    {firstname: 'saban', lastname: 'ü'}
+  ]);
 
   constructor() { }
-
-
   add( user: User ): User {
-    this.userList.push( user );
+    this.userList$.value.push( user );
+    this.userList$.next( this.userList$.value );
     return user;
   }
 
   update( user: User, firstname: string, lastname: string ): User|null {
-    const ind = this.userList.indexOf( user );
+    const ind = this.userList$.value.indexOf( user );
     if ( ind !== -1 ) {
       user = {...user, firstname, lastname};
-      this.userList [ind] = user;
+      this.userList$.value [ind] = user;
+      this.userList$.next( this.userList$.value );
       return user;
     }
     return null;
   }
 
   delete( user: User ): boolean  {
-    const ind = this.userList.indexOf( user );
+    const ind = this.userList$.value.indexOf( user );
     if ( ind !== -1 ) {
-      this.userList.splice( ind, 1 );
+      this.userList$.value.splice( ind, 1 );
+      this.userList$.next( this.userList$.value );
       return true;
     }
     return false;
